a9b2ee784b27e769edfde85bbc6cca01ad578893
eaglegokul <gokulgurusamy99@gmail.com>
2022-11-21 19:21:48 +0530

    changes made by bat

--- src/org/practice/Final.java
+++ src/org/practice/Final.java
@@ -2,6 +2,6 @@ package org.practice;
 
 public class Final {
 public static void main(String[] args) {
-	System.out.println("program");
+	System.out.println("program");System.out.println("program");
 }
 }
